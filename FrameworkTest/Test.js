5c434282f5d6720cc51b511073507ed598537246
L.Gabrielse <leroy.gabrielse@hva.nl>
2020-04-29 01:02:34 +0200

    etst

--- FrameworkTest/Test.js
+++ FrameworkTest/Test.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default class Test extends Component {
     render() {
         return (
-            <View>
+            <View style = {styles.container}>
                 <Text> textInComponent </Text>
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#fcc"
+    }
+});
+
